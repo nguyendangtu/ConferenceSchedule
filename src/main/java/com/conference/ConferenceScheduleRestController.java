7c3eac92710d6440d618b0b6f74d22ff09bbea9a
JOHNNGUYEN <nguyendangtu1987@gmail.com>
2019-01-22 00:54:37 +0800

    add controller to public result

--- src/main/java/com/conference/ConferenceScheduleRestController.java
+++ src/main/java/com/conference/ConferenceScheduleRestController.java
@@ -1,21 +1,24 @@
 package com.conference;
 
+import com.conference.beans.Talk;
 import com.conference.configurations.EventConfiguration;
 import com.conference.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
+
 @RestController
-public class RestControllerExample {
+public class ConferenceScheduleRestController {
     @Autowired
     private EventConfiguration eventConfiguration;
     @Autowired
     private ScheduleService scheduleService;
 
     @GetMapping("/")
-    public String hello() throws Exception {
-        scheduleService.createSchedule();
-        return "Hello ";
+    public Map<String, List<Talk>> hello() throws Exception {
+        return scheduleService.createSchedule();
     }
 }
